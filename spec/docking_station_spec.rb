315f7fc577dc61a0d20181f3d0bf75634ec57e9d
Suzanne Huldt <suzanne.huldt.16@ucl.ac.uk>
2017-10-25 15:30:21 +0100

    docking tests

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,9 +1,13 @@
 require "docking_station"
 
 describe DockingStation do
-  it 'responds to release_bike' do
-    expect(release_bike(DockingStation)).to eq 'respond_to'
-  end
+  it {is_expected.to be_a(DockingStation)}
 
-
+  it { is_expected.to respond_to(:release_bike) }
+  it 'create a bike' do
+    expect(subject.release_bike).to be_instance_of(Bike)
 end
+it do  expect(subject).to respond_to(:dock).with(1).argument
+end
+
+  end
